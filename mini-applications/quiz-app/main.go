f02ba55dcb1c81f8a7d1ca38d926e47e2a9b9d18
Tanmay Awasthi <tanmayawasthi105@gmail.com>
2020-04-19 13:25:32 +0530

    Added feature to close the test on a timeout

--- mini-applications/quiz-app/main.go
+++ mini-applications/quiz-app/main.go
@@ -6,11 +6,19 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
 	csvFile := flag.String("csv", "mini-applications/quiz-app/problems.csv", "a csv file of format 'question, answer'")
+	timer := flag.String("timeout", "2s", "time per question in seconds")
 	flag.Parse()
+
+	duration, err := time.ParseDuration(*timer)
+
+	if err != nil {
+		exit("Timeout value is not in right format, use values like 2s, 4m, 1h", 1)
+	}
 
 	file, err := os.Open(*csvFile)
 	if err != nil {
@@ -25,21 +33,40 @@ func main() {
 	}
 
 	problems := parseCsv(lines)
-	correct := startTest(problems)
+	correct := startTest(problems, duration)
 	fmt.Println("You scored ", correct, "/", len(problems))
 }
 
-func startTest(p []problems) int {
+func startTest(p []problems, timer time.Duration) int {
+	ch := make(chan string, 1)
+
 	correct := 0
 	for i, problem := range p {
 		fmt.Printf("Problem %d: %s =\n", i+1, problem.q)
-		var input string
-		fmt.Scanln(&input) //this will trim the spaces. No need to explicit trim
-		if input == problem.a {
-			correct++
+		go read(ch)
+
+		select {
+		case input := <-ch:
+			{
+				if input == problem.a {
+					correct++
+				}
+			}
+		case <-time.After(timer):
+			{
+				close(ch)
+				fmt.Println("Too late. Exiting!")
+				return correct
+			}
 		}
 	}
 	return correct
+}
+
+func read(c chan string) {
+	var input string
+	_, _ = fmt.Scanln(&input) //this will trim the spaces. No need to explicit trim
+	c <- input
 }
 
 func parseCsv(lines [][]string) []problems {
